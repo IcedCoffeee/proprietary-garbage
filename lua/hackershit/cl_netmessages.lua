0f14fed2976a85df6bc92a5879c796ec187eef13
Iced Coffee <me@icedd.coffee>
2017-03-16 09:45:20 +1100

    4got 2 delete

--- lua/hackershit/cl_netmessages.lua
+++
@@ -1,9 +0,0 @@
-net.Receive("MyIP", function()
-	IP = net.ReadString()
-	CurrentDir = net.ReadTable()
-end)
-
-net.Receive("DirResponse", function()
-	CurrentDir = net.ReadTable()
-	reloadfiles()
-end)
